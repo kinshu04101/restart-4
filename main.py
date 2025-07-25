da23264c1539e77b05822426385f08dfbb11d4ad
kinshu04101 <145156287+kinshu04101@users.noreply.github.com>
2025-07-26 01:41:32 +0530

    Upload or update main.py

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ async def open_and_screenshot_urls():
                 full_path = os.path.abspath(filename)
                 for chat_id in CHAT_IDS:
                     await app.send_message(chat_id=chat_id, text=f"📦 Screenshot `{full_path}` size: `{file_size}` bytes")
-                    #await app.send_photo(chat_id=chat_id, photo=full_path, caption=f"📄 Screenshot for `{url}` (session #{session_index})")
+                    await app.send_document(chat_id=chat_id, document=full_path, caption=f"📄 Screenshot for `{url}`")
 
                 # Uncomment below after debugging:
                 # os.remove(filename)
